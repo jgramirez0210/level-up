1a6521375681b352162ad4776c0199137cfb4c60
Jesse Ramirez <gideonramirezz@gmail.com>
2024-05-24 15:30:31 -0500

    first commit

---
+++ .history/levelupapi/views/EventView_20240523130634.py
@@ -0,0 +1,40 @@
+from django.http import HttpResponseServerError
+from rest_framework.viewsets import ViewSet
+from rest_framework.response import Response
+from rest_framework import serializers, status
+from levelupapi.models import Event
+
+
+class EventView(ViewSet):
+    """Level up event types view"""
+
+    def retrieve(self, request, pk):
+        """Handle GET requests for single event type
+
+        Returns:
+            Response -- JSON serialized event type
+        """
+        event = Event.objects.get(pk=pk)
+        serializer = EventViewSerializer(event)
+        return Response(serializer.data)
+
+    def list(self, request):
+        """Handle GET requests to get all events
+    
+        Returns:
+            Response -- JSON serialized list of events
+        """
+        events = Event.objects.all()
+        event = request.query_params.get('event', None)
+        if events is not None:
+            events = events.filter(event_id=event)
+    
+        serializer = EventViewSerializer(events, many=True)
+        return Response(serializer.data)
+
+class EventViewSerializer(serializers.ModelSerializer):
+    """JSON serializer for event types
+    """
+    class Meta:
+        model = Event
+        fields = ('id', 'game', 'description', 'date', 'time', 'organizer')
---
+++ levelupapi/views/GameView.py
@@ -0,0 +1,59 @@
+from django.http import HttpResponseServerError
+from rest_framework.viewsets import ViewSet
+from rest_framework.response import Response
+from rest_framework import serializers, status
+from levelupapi.models import Game, Gamer, Game_Type
+
+
+class GameView(ViewSet):
+    """Level up game types view"""
+
+    def retrieve(self, request, pk):
+        """Handle GET requests for single game type
+
+        Returns:
+            Response -- JSON serialized game type
+        """
+        game = Game.objects.get(pk=pk)
+        serializer = GameSerializer(game)
+        return Response(serializer.data)
+
+    def list(self, request):
+        """Handle GET requests to get all game types
+
+        Returns:
+            Response -- JSON serialized list of game types
+        """
+        games = Game.objects.all()
+        game_type = request.query_params.get("type", None)
+        if game_type is not None:
+            games = games.filter(game_type__id=game_type)
+        serializer = GameSerializer(games, many=True)
+        return Response(serializer.data)
+        
+    def create(self, request):
+        """Handle POST operations   
+
+        Returns
+            Response -- JSON serialized game instance
+        """
+        gamer = Gamer.objects.get(uid=request.data["userId"])
+        game_type = Game_Type.objects.get(pk=request.data["gameType"])   
+
+        game = Game.objects.create(
+            title=request.data["title"],
+            maker=request.data["maker"],
+            number_of_players=request.data["numberOfPlayers"],
+            skill_level=request.data["skillLevel"],
+            game_type=game_type,
+            gamer=gamer,
+        )
+        serializer = GameSerializer(game)
+        return Response(serializer.data) 
+
+class GameSerializer(serializers.ModelSerializer):
+    """JSON serializer for game types
+    """
+    class Meta:
+        model = Game
+        fields = ('id', 'game_type', 'title', 'maker', 'number_of_players', 'skill_level')
